0651fdf7ba3fef97584c220760e8c8956c7cee16
Nam G VU <namgivu@gmail.com>
2023-03-14 14:16:02 +0700

    /translate @ skip outputprefix by test offset

--- api/demo_web_app.py
+++ api/demo_web_app.py
@@ -105,11 +105,15 @@ def demo_web_app(gpt, config=UIConfig()):
     @app.route('/translate', methods=['GET', 'POST'])
     def translate():
         # pylint: disable=unused-variable
-        prompt = request.json["prompt"]
+        prompt   = request.json['prompt']
         response = gpt.submit_request(prompt)
+
+        #region skip outputprefix by test offset
         offset = 0
         if not gpt.append_output_prefix_to_query:
             offset = len(gpt.output_prefix)
+        #endregion skip outputprefix by test offset
+
         return {'text': response['choices'][0]['text'][offset:]}
 
     #endregion flask apiapp endpoint(s)
